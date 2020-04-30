bfa97084f0a5f07941110af1110807b3aad9098d
IlyaEz <ilya.leviossa@gmail.com>
2020-04-30 11:39:17 +0300

    reload project

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,17 @@ import { NavLink } from 'react-router-dom';
  
 
 class Header extends Component { 
+  constructor(){
+    super()
+    this.state ={
+      isAuth: false
+    }
+  }
+
+  onClick = () => {
+    localStorage.clear()
+    this.setState({isAuth:!this.state.isAuth})
+  }
  render(){ 
     return (
       <div className="header"> 
@@ -11,7 +22,7 @@ class Header extends Component {
           <NavLink to='/profilepage'>Profile Page</NavLink>
           <NavLink to='/loginpage'>LogIn/SignIn</NavLink> 
           {
-            this.props.isAuth && <button onClick={()=>  localStorage.clear() }>LogOut</button> 
+            <button onClick={this.onClick}>LogOut</button> 
           }
       </div>
     );
